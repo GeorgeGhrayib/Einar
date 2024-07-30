c10c51fc58af3dc367260a72989808fe324022b5
GeorgeGhrayib <georgeghrayib2008@gmail.com>
2024-07-30 17:57:29 +0300

    hopefully final commit

--- app.py
+++ app.py
@@ -120,6 +120,12 @@ def home():
 def about():
     if request.method == 'GET':
         return render_template("about.html") 
+    else:
+        name = request.form['name']
+        message = request.form['message']
+        email = request.form['email']
+        db.child("contact_requests").push([name,message,email])
+        return redirect(url_for('about'))
     
 if __name__ == '__main__':
     app.run(debug=True)
